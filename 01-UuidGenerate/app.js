4c7a64c39e32085d1b675f06601f5494194525f2
furkankahvecii <furkankahveci@hotmail.com.tr>
2020-08-10 20:48:19 +0300

    Router name changed and added automatic orieantation.

--- 01-UuidGenerate/app.js
+++ 01-UuidGenerate/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const uuidController = require('./controllers/uuidController')
+const uuidController = require('./router/uuidRouter')
 
 // set up template engine
 app.set("view engine","ejs")
@@ -8,8 +8,13 @@ app.set("view engine","ejs")
 // static files
 app.use(express.static("./public"))
 
-//fire controllers
-uuidController(app)
+// uuid router
+app.use('/uuid',uuidController)
+
+// automatic orientation
+app.use('*', (req,res)=>{
+    res.redirect('http://localhost:3000/uuid');
+})
 
 // listen to port
 app.listen(3000 , () => { console.log("Server started...")})
